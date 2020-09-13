df8c192f79fad1b45893fed4748114d01512100d
Dmitry Statsura <kekusdes@gmail.com>
2020-09-13 17:42:45 +0300

    fix

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Navigator from './src/navigation';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={{ flex: 1 }}>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Navigator />
-      </NavigationContainer>
-    </View>
-  );
+export default () => {
+	return (
+		<View style={{ flex: 1 }}>
+			<StatusBar style='auto' />
+			<NavigationContainer>
+				<Navigator />
+			</NavigationContainer>
+		</View>
+	);
 }
-
